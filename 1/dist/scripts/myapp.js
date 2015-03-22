3c1523d560b1f37e82ed61e0d9a1e8495d67e05c
stamaimer <stamaimer@gmail.com>
2015-03-22 11:56:04 +0800

    merge gallery and footer

---
+++ 1/dist/scripts/myapp.js
@@ -0,0 +1,10 @@
+var myApp = angular.module('myApp', ['ui.bootstrap']);
+
+myApp.controller('CarouselCtrl', function ($scope) {
+	var slides = $scope.slides = [{image:"images/SisterswStaciMonica.jpg"},
+								  {image:"images/Truvee+Old+Vine+Grenache.jpg"},
+								  {image:"images/A001_C017_1204XZ.0000549.jpeg"},
+								  {image:"images/16706540385_75fa074e59_o.jpg"},
+								  {image:"images/truvee+chardonnay.jpg"}];
+
+});
