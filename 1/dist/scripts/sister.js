4faeba45f43dce58b0bd1e998a5aaddcbef2b6f3
Carwest Sung <carwestsam@gmail.com>
2015-03-28 01:28:39 +0800

    form added

--- 1/dist/scripts/sister.js
+++ 1/dist/scripts/sister.js
@@ -25,6 +25,7 @@ truveeApp
 	$scope.Env.pageHeights = []
 	for ( ids in $scope.Env.pageIds ){
 		var t = $scope.Env.pageIds[ids];
+		console.log( t);
 		console.log(t, document.getElementById(t), document.getElementById(t).getBoundingClientRect().top);
 		$scope.Env.pageHeights.push( document.getElementById(t).getBoundingClientRect().top );
 	}
@@ -155,4 +156,58 @@ truveeApp
     	var ele = document.getElementById(eleId);
     	$document.scrollToElement( ele, 0, 1500 );
     }
+}])
+.controller('ContactCtrl', ['$scope', function($scope){
+	$scope.Contact = {};
+	$scope.Contact.firstName = "";
+	$scope.Contact.lastName = "";
+	$scope.Contact.email = "";
+	$scope.Contact.subject = "";
+	$scope.Contact.message = "";
+
+	$scope.ContactCtrl = {};
+	$scope.ContactCtrl.btnText = "Submit";
+
+	$scope.Error = {}
+	$scope.Error.name = 0;
+	$scope.Error.email = 0;
+	$scope.Error.sub = 0;
+	$scope.Error.msg = 0;
+	$scope.Error.form = 0;
+
+	$scope.ContactCtrl.btnClickFunc = function(){
+		$scope.ContactCtrl.btnText = "Submitting...";
+		if ( $scope.Contact.firstName.length > 0 || $scope.Contact.lastName.length > 0 ){
+			$scope.Error.name = 0;
+		}else {
+			$scope.Error.name = 1;
+		}
+
+		var check = function( str ){
+			console.log( str, typeof(str), typeof(str) == "undefined")
+			if ( typeof(str) == "undefined" ){
+				return 2;
+			}else if ( str.length > 0 ){
+				return 0;
+			}else {
+				return 1;
+			}
+		}
+
+		console.log( $scope.Contact, $scope.Error )
+		$scope.Error.email = check( $scope.Contact.email )
+		$scope.Error.sub = check( $scope.Contact.subject )
+		$scope.Error.msg = check( $scope.Contact.message )
+		
+
+		console.log( $scope.Contact, $scope.Error )
+		if ( $scope.Error.name == 0 && $scope.Error.email == 0 && $scope.Error.sub == 0 && $scope.Error.msg == 0 ){
+			$scope.Error.form = 0;
+			$scope.ContactCtrl.btnText = "Submit";
+		}else {
+			$scope.Error.form = 1;
+			
+		}
+	}
+	console.log('hello Contact');
 }]);
